7f0097046ccb2b38175d15c0439fcb47c0cc3b85
apiwatdev <82103342+apiwatdev@users.noreply.github.com>
2022-11-04 11:09:59 +0700

    getting start graphql js : add some custom

--- graphQL-js/server.js
+++ graphQL-js/server.js
@@ -3,15 +3,26 @@ const { graphql, buildSchema } = require("graphql");
 const schema = buildSchema(
   `
     type Query {
-        hello: String
+        hello: String,
+        hello_from_TH: String,
+        goodbye: String
     }
     `
 );
 
-const rootValue = { hello: () => "Hello world" };
+const rootValue = {
+  hello: () => "Hello world",
+  hello_from_TH: () => "sawaddee",
+  goodbye: () => "good bye",
+};
 
-const source = `{ hello }`;
+const source = `{ hello, hello_from_TH }`;
 
 graphql({ schema, source, rootValue }).then((response) => {
+  console.log(response);
+});
+
+
+graphql({ schema, source : `{goodbye}`, rootValue }).then((response) => {
   console.log(response);
 });
